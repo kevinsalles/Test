9817be8f4ed7b1c8d9bd95c659c6e388cf33e619
kevinsalles <kvin.salles@gmail.com>
2015-09-21 22:43:09 -0400

    start project

--- js/animation.js
+++ js/animation.js
@@ -1,3 +1,13 @@
 /**
  * Created by Kevin on 2015-09-21.
  */
+
+function Active(){
+    this.div = document.getElementsByClassName("carre");
+    var left = 0;
+    while(left < 800)
+    {
+        left += 1;
+        this.div[0].style.left = left + "px";
+    }
+}
