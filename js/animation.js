13d5f58031e2f9cd17acc722633758067086d456
kevinsalles <kvin.salles@gmail.com>
2015-09-21 23:47:13 -0400

    finish

--- js/animation.js
+++ js/animation.js
@@ -3,45 +3,64 @@
  */
 var left = 0;
 var width = 100;
+var speed = 1;
+var velocity1 = 20;
+var velocity2 = 20;
+var anim = false;
 
 function Active(){
     this.div = document.getElementsByClassName("carre");
 
     if(width  < 900 && left == 0)
     {
-        width += 1;
+        width += velocity1;
         this.div[0].style.width = width + "px";
-        time1 = setTimeout(Active,5);
+        setTimeout(Active,speed);
     }
     else if(left < 800)
     {
-        clearTimeout(time1);
-        left += 1;
-        width -= 1;
+        left += velocity1;
+        width -= velocity1;
         this.div[0].style.left = left + "px";
         this.div[0].style.width = width + "px";
-        time2 = setTimeout(Active,5);
+        setTimeout(Active,speed);
     }
-    else{
-        clearTimeout(time2);
+    else if(anim)
+    {
+        setTimeout(Init,speed);
     }
 }
 
 function Init(){
     this.div = document.getElementsByClassName("carre");
 
-    if(left >= 800 && left > 0)
+    if(left > 0)
     {
-        left -= 1;
-        width += 1;
+        left -= velocity2;
+        width += velocity2;
         this.div[0].style.left = left + "px";
         this.div[0].style.width = width + "px";
-        setTimeout(Active,5);
+        setTimeout(Init,speed);
     }
-    else if(width > 100)
+    else if(width >= 100)
     {
-        width -= 1;
+        width -= velocity2;
         this.div[0].style.width = width + "px";
-        setTimeout(Active,5);
+        setTimeout(Init,speed);
     }
+    else if(anim)
+    {
+        setTimeout(Active,speed);
+    }
+}
+
+
+function Anim(){
+    anim = true;
+    Active();
+}
+
+function Stop(){
+    anim = false;
+    Init();
 }
